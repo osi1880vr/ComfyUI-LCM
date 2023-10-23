dd15e9d713968c200763d1e5a2d830d2d3e829ec
0xbitches <jasonborn3250@gmail.com>
2023-10-23 05:22:35 -0700

    add fp16 support

--- nodes.py
+++ nodes.py
@@ -10,43 +10,52 @@ from comfy.model_management import get_torch_device
 
 MAX_SEED = np.iinfo(np.int32).max
 
+
 def randomize_seed_fn(seed: int, randomize_seed: bool) -> int:
     if randomize_seed:
         seed = random.randint(0, MAX_SEED)
     return seed
 
+
 class LCMSampler:
 
     def __init__(self):
-        self.scheduler = LCMScheduler.from_pretrained(path.join(path.dirname(__file__), "scheduler_config.json"))
+        self.scheduler = LCMScheduler.from_pretrained(
+            path.join(path.dirname(__file__), "scheduler_config.json"))
         self.pipe = None
 
     @classmethod
     def INPUT_TYPES(s):
         return {"required":
-                    {
+                {
                     "seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff}),
                     "steps": ("INT", {"default": 4, "min": 1, "max": 10000}),
-                    "cfg": ("FLOAT", {"default": 8.0, "min": 0.0, "max": 100.0, "step":0.5, "round": 0.01}),
+                    "cfg": ("FLOAT", {"default": 8.0, "min": 0.0, "max": 100.0, "step": 0.5, "round": 0.01}),
                     "size": ("INT", {"default": 512, "min": 512, "max": 768}),
                     "num_images": ("INT", {"default": 1, "min": 1, "max": 64}),
-                     "positive_prompt": ("STRING", {"multiline": True}),
-                    }
+                    "use_fp16": ("BOOLEAN", {"default": True}),
+                    "positive_prompt": ("STRING", {"multiline": True}),
+                }
                 }
 
     RETURN_TYPES = ("IMAGE",)
     FUNCTION = "sample"
-
     CATEGORY = "sampling"
 
-    def sample(self, seed, steps, cfg, positive_prompt, size, num_images):
+    def sample(self, seed, steps, cfg, positive_prompt, size, num_images, use_fp16):
         if self.pipe is None:
             self.pipe = LatentConsistencyModelPipeline.from_pretrained(
                 pretrained_model_name_or_path="SimianLuo/LCM_Dreamshaper_v7",
                 local_files_only=True,
                 scheduler=self.scheduler
             )
-            self.pipe.to(get_torch_device())
+
+            if use_fp16:
+                self.pipe.to(torch_device=get_torch_device(),
+                             torch_dtype=torch.float16)
+            else:
+                self.pipe.to(torch_device=get_torch_device(),
+                             torch_dtype=torch.float32)
 
         torch.manual_seed(seed)
         start_time = time.time()
@@ -67,10 +76,11 @@ class LCMSampler:
 
         return (images_tensor,)
 
+
 NODE_CLASS_MAPPINGS = {
     "LCMSampler": LCMSampler
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "LCMSampler" : "LCMSampler"
+    "LCMSampler": "LCMSampler"
 }
