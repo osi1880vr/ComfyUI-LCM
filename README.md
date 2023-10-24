1e430319fd9682c266cba746ab0acebfa416ff02
0xbitches <jasonborn3250@gmail.com>
2023-10-24 04:55:39 -0700

    img2img and vid2vid

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # Latent Consistency Model for ComfyUI
 
-![Context Node](./preview.png)
+![Context Node](./assets/preview.png)
 
 This extension aims to integrate [Latent Consistency Model (LCM)](https://latent-consistency-models.github.io/) into [ComfyUI](https://github.com/comfyanonymous/ComfyUI).
 
-Note that LCMs are a completely different class of models than Stable Diffusion, and the only available checkpoint currently is [LCM_Dreamshaper_v7](https://huggingface.co/SimianLuo/LCM_Dreamshaper_v7). Due to this, this implementation uses the [diffusers](https://huggingface.co/docs/diffusers/index) library, and not Comfy's own model loading mechanism. 
+Note that LCMs are a completely different class of models than Stable Diffusion, and the only available checkpoint currently is [LCM_Dreamshaper_v7](https://huggingface.co/SimianLuo/LCM_Dreamshaper_v7). Due to this, this implementation uses the [diffusers](https://huggingface.co/docs/diffusers/index) library, and not Comfy's own model loading mechanism.
 
-## Installation:
+## Installation
+
 Simply clone this repo to your `custom_nodes/` directory:
 
 ```
@@ -15,11 +16,24 @@ git clone https://github.com/0xbitches/ComfyUI-LCM
 
 Then restart ComfyUI.
 
+## Img2Img / Vid2Vid
+
+![vid2vid](./assets/vid2vid.gif)
+
+For basic img2img, you can just use the `LCM_img2img_Sampler` node.
+
+For vid2vid, you will want to install this helper node:
+[ComfyUI-VideoHelperSuite](https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite).
+
+Then, use the `Load Video` and `Video Combine` nodes to create a vid2vid workflow, or download [this workflow](./assets/lcm_vid2vid.json).
+
+Huge thanks to [nagolinc](https://github.com/nagolinc) for implementing the pipeline.
+
 ## Known Issues
 
 #### `ValueError: Non-consecutive added token '<|startoftext|>' found. Should have index 49408 but has index 49406 in saved vocabulary.`
 
-To resolve this, locate your huggingface hub cache directory. 
+To resolve this, locate your huggingface hub cache directory.
 
 It will be something like `~/.cache/huggingface/hub/path_to_lcm_dreamshaper_v7/tokenizer/`. On Windows, it will roughly be `C:\Users\YourUserName\.cache\huggingface\hub\models--SimianLuo--LCM_Dreamshaper_v7\snapshots\c7f9b672c65a664af57d1de926819fd79cb26eb8\tokenizer\`.
 
@@ -31,4 +45,3 @@ Find the file `added_tokens.json` and change the contents to:
   "<|startoftext|>": 49408
 }
 ```
-
--- nodes.py
+++ nodes.py
@@ -1,5 +1,6 @@
 from .lcm.lcm_scheduler import LCMScheduler
 from .lcm.lcm_pipeline import LatentConsistencyModelPipeline
+from .lcm.lcm_i2i_pipeline import LatentConsistencyModelImg2ImgPipeline
 from os import path
 import time
 import torch
@@ -16,8 +17,7 @@ def randomize_seed_fn(seed: int, randomize_seed: bool) -> int:
     return seed
 
 
-class LCMSampler:
-
+class LCM_Sampler:
     def __init__(self):
         self.scheduler = LCMScheduler.from_pretrained(
             path.join(path.dirname(__file__), "scheduler_config.json"))
@@ -45,7 +45,6 @@ class LCMSampler:
         if self.pipe is None:
             self.pipe = LatentConsistencyModelPipeline.from_pretrained(
                 pretrained_model_name_or_path="SimianLuo/LCM_Dreamshaper_v7",
-                local_files_only=True,
                 scheduler=self.scheduler
             )
 
@@ -76,10 +75,83 @@ class LCMSampler:
         return (images_tensor,)
 
 
+
+class LCM_img2img_Sampler:
+    def __init__(self):
+        self.scheduler = LCMScheduler.from_pretrained(
+            path.join(path.dirname(__file__), "scheduler_config.json"))
+        self.pipe = None
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required":
+                {
+                    "images": ("IMAGE", ),
+                    "seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff}),
+                    "prompt_strength": ("FLOAT", {"default": 0.5, "min": 0.0, "max": 1.0, "step": 0.05}),
+                    "steps": ("INT", {"default": 4, "min": 1, "max": 10000}),
+                    "cfg": ("FLOAT", {"default": 8.0, "min": 0.0, "max": 100.0, "step": 0.5, "round": 0.01}),
+                    "height": ("INT", {"default": 512, "min": 512, "max": 768}),
+                    "width": ("INT", {"default": 512, "min": 512, "max": 768}),
+                    "num_images": ("INT", {"default": 1, "min": 1, "max": 64}),
+                    "use_fp16": ("BOOLEAN", {"default": True}),
+                    "positive_prompt": ("STRING", {"multiline": True}),
+                }
+                }
+
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "sample"
+    CATEGORY = "sampling"
+
+    def sample(self, seed, steps, prompt_strength, cfg, images, positive_prompt, height, width, num_images, use_fp16):
+        if self.pipe is None:
+            self.pipe = LatentConsistencyModelImg2ImgPipeline.from_pretrained(
+                pretrained_model_name_or_path="SimianLuo/LCM_Dreamshaper_v7",
+                safety_checker=None,
+            )
+
+            if use_fp16:
+                self.pipe.to(torch_device=get_torch_device(),
+                             torch_dtype=torch.float16)
+            else:
+                self.pipe.to(torch_device=get_torch_device(),
+                             torch_dtype=torch.float32)
+
+        torch.manual_seed(seed)
+        start_time = time.time()
+
+        images = np.transpose(images, (0, 3, 1, 2))
+        results = []
+        for i in range(images.shape[0]):
+            image = images[i]
+            result = self.pipe(
+                image=image,
+                prompt=positive_prompt,
+                strength=prompt_strength,
+                width=width,
+                height=height,
+                guidance_scale=cfg,
+                num_inference_steps=steps,
+                num_images_per_prompt=num_images,
+                lcm_origin_steps=50,
+                output_type="np",
+                ).images
+            tensor_results = [torch.from_numpy(np_result) for np_result in result]
+            results.extend(tensor_results)
+
+        results = torch.stack(results)
+        
+        print("LCM img2img inference time: ", time.time() - start_time, "seconds")
+
+        return (results,)
+
+
 NODE_CLASS_MAPPINGS = {
-    "LCMSampler": LCMSampler
+    "LCM_Sampler": LCM_Sampler,
+    "LCM_img2img_Sampler": LCM_img2img_Sampler
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "LCMSampler": "LCMSampler"
+    "LCM_Sampler": "LCM Sampler",
+    "LCM_img2img_Sampler": "LCM img2img Sampler"
 }
